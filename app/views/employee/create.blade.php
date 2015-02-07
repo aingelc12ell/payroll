27d15ba4be415d43ad4e0e7a14af8950db57aade
buonzz <buonzz@gmail.com>
2015-02-08 07:55:55 +0800

    added new fields

--- app/views/employee/create.blade.php
+++ app/views/employee/create.blade.php
@@ -6,6 +6,20 @@
 	@endif
 	
 	{{ Form::open(array('route' => 'employees.store')) }}
+	 
+	  <div class="form-group 
+	  					@if($errors->has('employee_number'))
+		  					has-warning
+						@endif
+		  	">
+	    <label for="employee_number">Employee Number</label>
+		{{ Form::text('employee_number', null, array('class'=>'form-control', 'placeholder' => 'Enter Employee Number')) }}
+		@if($errors->has('employee_number'))
+			<span class="help-inline">{{ $errors->first('employee_number') }}</span>
+		@endif
+	  </div>
+	  
+	  
 	  <div class="form-group 
 	  					@if($errors->has('firstname'))
 		  					has-warning
@@ -17,6 +31,7 @@
 			<span class="help-inline">{{ $errors->first('firstname') }}</span>
 		@endif
 	  </div>
+	  
 	  <div class="form-group @if($errors->has('middlename'))
 		  						has-warning
 							 @endif">
@@ -37,6 +52,84 @@
 		
 		@if($errors->has('lastname'))
 			<span class="help-inline">{{ $errors->first('lastname') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="radio">
+	  	<label for="Gender"></label>
+		{{ Form::radio('gender', 'male') }} Male <br/>
+		{{ Form::radio('gender', 'female') }} Female
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('birthdate'))
+		  						has-warning
+							@endif">
+	    <label for="birthdate">Birth Date</label>
+		
+		{{ Form::text('birthdate', null, array('class'=>'form-control', 'placeholder' => 'Enter Birth Date')) }}
+		
+		@if($errors->has('birthdate'))
+			<span class="help-inline">{{ $errors->first('birthdate') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('phone'))
+		  						has-warning
+							@endif">
+	    <label for="birthdate">Phone</label>
+		
+		{{ Form::text('phone', null, array('class'=>'form-control', 'placeholder' => 'Enter Phone')) }}
+		
+		@if($errors->has('phone'))
+			<span class="help-inline">{{ $errors->first('phone') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('Address'))
+		  						has-warning
+							@endif">
+	    <label for="address">Address</label>
+		
+		{{ Form::text('address', null, array('class'=>'form-control', 'placeholder' => 'Enter Address')) }}
+		
+		@if($errors->has('address'))
+			<span class="help-inline">{{ $errors->first('address') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('zip'))
+		  						has-warning
+							@endif">
+	    <label for="birthdate">Zip</label>
+		
+		{{ Form::text('zip', null, array('class'=>'form-control', 'placeholder' => 'Enter Zip')) }}
+		
+		@if($errors->has('zip'))
+			<span class="help-inline">{{ $errors->first('zip') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('email'))
+		  						has-warning
+							@endif">
+	    <label for="birthdate">email</label>
+		
+		{{ Form::text('email', null, array('class'=>'form-control', 'placeholder' => 'Enter Email')) }}
+		
+		@if($errors->has('email'))
+			<span class="help-inline">{{ $errors->first('email') }}</span>
+		@endif
+	  </div>
+	  
+	  <div class="form-group @if($errors->has('phone'))
+		  						has-warning
+							@endif">
+	    <label for="birthdate">Phone</label>
+		
+		{{ Form::text('phone', null, array('class'=>'form-control', 'placeholder' => 'Enter Phone')) }}
+		
+		@if($errors->has('phone'))
+			<span class="help-inline">{{ $errors->first('phone') }}</span>
 		@endif
 	  </div>
 	  <button type="submit" class="btn btn-default">Save</button>
