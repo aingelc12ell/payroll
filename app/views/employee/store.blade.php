888e62e73be24765f5434f8e02c3127ce4464912
buonzz <buonzz@gmail.com>
2015-02-08 04:46:50 +0800

    add commit message

--- app/views/employee/store.blade.php
+++ app/views/employee/store.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.page')
 
 @section('content')
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <h1>Payroll System Demo</h1>
-      <p>This is a demo application for Payroll System by Biler IT Solutions.</p>
-    </div>
+   <div class="alert alert-success" role="alert">
+	   Employee Success fully Added!<br/>
+	  {{ link_to('employees.create', 'Add Another') }}
+   </div>
 @stop
