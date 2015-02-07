efd763ad9a9b7a195d3c8a216f7b6c329d0b0645
buonzz <buonzz@gmail.com>
2015-02-08 05:55:45 +0800

    error validation

--- app/views/employee/create.blade.php
+++ app/views/employee/create.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.page')
 
 @section('content')
+	@if($errors->has())
+		<div class="alert alert-warning" role="alert">Please check the form for errors</div>
+	@endif
+	
 	{{ Form::open(array('route' => 'employees.store')) }}
-	  <div class="form-group">
+	  <div class="form-group 
+	  					@if($errors->has('txtFirstName'))
+		  					warning
+						@endif
+		  	">
 	    <label for="txtFirstName">First Name</label>
 	    <input type="text" class="form-control" id="txtFirstName" name="txtFirstName" placeholder="Enter First Name">
+		@if($errors->has('txtFirstName'))
+			<span class="help-inline">{{ $errors->first('txtFirstName') }}</span>
+		@endif
 	  </div>
 	  <div class="form-group">
 	    <label for="txtMiddleName">Middle Name</label>
